a8d3e60b3f728179ab5afa838cfecb42b3ad8565
MDhanesh <mgdhanesh98@gmail.com>
2022-12-08 11:03:51 +0530

    updated link in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -67,7 +67,7 @@ const Header = () => {
           </li>
           <li className="mx-5 text-lg">
             <NavLink
-              to={"/login"}
+              to={""}
               className={({ isActive }) =>
                 isActive ? isActiveStyle : isnotActiveStyle
               }
@@ -77,7 +77,7 @@ const Header = () => {
           </li>
           <li className="mx-5 text-lg">
             <NavLink
-              to={"/"}
+              to={""}
               className={({ isActive }) =>
                 isActive ? isActiveStyle : isnotActiveStyle
               }
@@ -117,7 +117,7 @@ const Header = () => {
                   <hr />
                 </>
               )}
-              <NavLink to={"/userProfile"}>
+              <NavLink to={{}}>
                 <p className="text-base text-textColor hover:font-semibold duration-150 transition-all ease-in-out">
                   Profile
                 </p>
--- src/utils/supportfunctions.js
+++ src/utils/supportfunctions.js
@@ -15,14 +15,3 @@ export const filterByLanguage = [
   { id: 4, name: "Telungu", value: "Telungu" },
   { id: 5, name: "Hindi", value: "hindi" },
 ];
-
-// export const deleteAnObject = (referenceUrl) => {
-//   const deleteRef = ref(storage, referenceUrl);
-//   deleteObject(deleteRef)
-//     .then(() => {
-//       return true;
-//     })
-//     .catch((error) => {
-//       return false;
-//     });
-// };
